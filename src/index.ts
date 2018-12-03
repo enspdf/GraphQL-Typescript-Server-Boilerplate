84c0f474c4cdfaef14100b8b9d9ff20fa71f3e9d
Sebastian Higuita <sebastianhc1995@hotmail.com>
2018-12-02 19:15:26 -0500

    Test Register Mutation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import "reflect-metadata";
-import { importSchema } from "graphql-import";
-import { GraphQLServer } from "graphql-yoga";
-import { resolvers } from "./resolvers";
+import { startServer } from "./startServer";
 
-const typeDefs = importSchema("schema.graphql");
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+startServer();
---
+++ src/tests/register.test.ts
@@ -0,0 +1,31 @@
+import { request } from "graphql-request";
+import { User } from "../entity/User";
+import { startServer } from "../startServer";
+import { AddressInfo } from "net";
+
+let getHost = () => "";
+
+beforeAll(async () => {
+  const app = await startServer();
+  const { port } = app.address() as AddressInfo;
+  getHost = () => `http://127.0.0.1:${port}`;
+});
+
+const email = "tom@bob.com";
+const password = "kajsdag";
+
+const mutation = `
+    mutation {
+        register(email: "${email}", password: "${password}")
+    }
+`;
+
+test("Register User", async () => {
+  const response = await request(getHost(), mutation);
+  expect(response).toEqual({ register: true });
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
